95e3bd5d13350deecb46244a55427167d960761d
mason-programming <kirb5233@fredonia.edu>
2021-07-25 14:01:28 -0400

    sprint HW

--- hw2-sprint1.cpp
+++ hw2-sprint1.cpp
@@ -5,9 +5,13 @@
 using namespace std; 
 
 carRental* insertInfo();
-
+void printAllVehicles();
+void menu();
 
 int main(){
+
+    truck* newTruck = new truck();
+    newTruck->setVechicalInfo("tuck","ford",5,6);
 
     car* newCar = new car(); 
     newCar->setVechicalInfo("sedan","Nissan",5,4);
@@ -17,6 +21,24 @@ int main(){
     return 0; 
 }
 
+void menu(){
+    int keyStroke = 0; 
+    cout << "Enter a menu option"<< "\n" << "Press 2 to print all cars" << "\n" << "Press 3 to select which capcity of car to print" << endl; 
+    if(keyStroke == 2){
+       printAllVehicles();
+    }else if(keyStroke == 3){
+        
+
+    }else{
+
+        cout << "Please choose from the list"<< endl;
+        menu(); 
+    }
+}
+void printAllVehicles(){
+    if()
+    for(int)
+}
 carRental* insertInfo(){
 
 }

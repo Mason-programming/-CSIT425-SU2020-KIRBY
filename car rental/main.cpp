756571d022417b8c888339a94e8302aacb80a31c
Mason Kirby <kirbm5233@fredonia.edu>
2021-08-04 19:16:19 -0400

    Initial Commit

---
+++ car rental/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  car rental
+//
+//  Created by Mason Kirby on 8/4/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
